ba04791b712f130b83a84d3236c54b8effdd72cc
Thejus Rajendran <thejusr1999@icloud.com>
2024-07-21 12:44:06 +0530

    - chore

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,11 +2,13 @@ import BannerText from "./BannerText";
 
 export default function HeroSection() {
   return (
-    <section className="h-screen p-12 pt-24 flex flex-col bg-neutral-900 text-neutral-50">
+    <section className="h-screen p-12 pt-24 flex flex-col bg-neutral-900 text-neutral-50 relative">
       <div className="h-full flex items-end text-2xl bg-red w-full md:w-2/5">
         <BannerText />
       </div>
-      <div className=" h-full flex items-end justify-center">Scroll Down</div>
+      <div className="z-10 h-full flex items-end justify-center">
+        Scroll Down
+      </div>
     </section>
   );
 }
